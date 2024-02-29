14f84ef6228fe6df1912ca19175045bbf004f2b4
somethingwithsony <somethingwithsony@gmail.com>
2024-02-29 17:12:47 +0000

    Render + Shader added

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -1,65 +1,66 @@
-    package renderEngine;
+package renderEngine;
+
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+import static org.lwjgl.system.MemoryUtil.*;
 
 
-    import org.lwjgl.opengl.*;
+public class DisplayManager {
+    private static long window;
 
 
-
-    import static org.lwjgl.glfw.GLFW.*;
-    import static org.lwjgl.opengl.GL11.*;
-
-    import static org.lwjgl.system.MemoryUtil.*;
-
-
-
-    public class DisplayManager {
-        private static long window;
-
-        public static void CreateDisplay(){
-            // Initialize GLFW. Most GLFW functions will not work before doing this.
-            glfwInit();
-
-            // Configure GLFW
-            glfwDefaultWindowHints(); // optional, the current window hints are already the default
-            glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-            glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-            // Create the window
-            window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-            if ( window == NULL )
-                throw new RuntimeException("Failed to create the GLFW window");
-
-            // Make the OpenGL context current
-            glfwMakeContextCurrent(window);
-            // Enable v-sync
-            glfwSwapInterval(1);
-
-            // Make the window visible
-            glfwShowWindow(window);
-
+    public static void CreateDisplay() {
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit()) {
+            // Handle initialization failure
+            throw new IllegalStateException("Unable to initialize GLFW");
         }
 
-        public static void UpdateDisplay(){
-            GL.createCapabilities();
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GL11.GL_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GL11.GL_FALSE);
 
-            // Set the clear color
-            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
 
-            glfwSwapBuffers(window); // swap the color buffers
+        // Create the window
+        window = glfwCreateWindow(1280, 720, "LWJGL + OpenGL", NULL, NULL);
 
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents();
-        }
 
-        public static void CloseDisplay(){
-            System.out.println("Closing Window");
-            glfwTerminate();
-        }
-        public static boolean CloseRequested(){
-            return glfwWindowShouldClose(window);
-        }
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+
+        GL.createCapabilities();
 
     }
+
+    public static void UpdateDisplay() {
+        glfwSwapBuffers(window); // swap the color buffers
+        glfwPollEvents();
+    }
+
+    public static void CloseDisplay() {
+        System.out.println("Closing Window");
+        glfwTerminate();
+    }
+
+    public static boolean CloseRequested() {
+        return glfwWindowShouldClose(window);
+    }
+
+}
